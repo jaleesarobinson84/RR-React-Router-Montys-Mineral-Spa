1b80acea2ce88f13453394a73565525b4973be0b
Jaleesa Robinson <robinson.jaleesa42@gmail.com>
2024-02-27 18:58:12 -0500

    completed activity

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav'
+import { Container } from 'react-bootstrap/Container';
 
 import Home from './components/Home'
 import About from './components/About'
@@ -19,6 +22,26 @@ function App() {
   return (
     <div className="App">
       <Router>
+      <Container>
+    <Nav defaultActiveKey="/" variant="tabs" fill>
+        <Nav.Item>
+            <Nav.Link href="/"> 
+                <Link to="/">Home</Link>
+            </Nav.Link>
+        </Nav.Item>
+        <Nav.Item >
+            <Nav.Link eventKey={"aboutPage"}> 
+                <Link to="/about">About Us</Link>
+            </Nav.Link>
+        </Nav.Item>
+        <Nav.Item >
+            <Nav.Link eventKey={"packagesPage"}> 
+                <Link to="/packages">Our Packages</Link> 
+            </Nav.Link>
+         </Nav.Item>
+    </Nav>
+</Container>
+
       <header>
         <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
